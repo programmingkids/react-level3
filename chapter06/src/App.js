63f9ea6b191952bd29d0db797327b6dbf48b6334
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2022-11-14 09:11:35 +0000

    added chapter06

---
+++ chapter06/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Title } from './components/blocks/Title';
+import { useGetItems } from './components/hooks/useGetItems';
+
+const App = () => {
+  const { items, onClickGetItems } = useGetItems();
+  
+  return (
+    <div className="App">
+      <Title>アイテム取得</Title>
+      <div>
+        <button onClick={onClickGetItems}>アイテム取得</button>
+        <div>
+          {items.map(item => <p key={item.id}>{item.id} : {item.name}</p>)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
